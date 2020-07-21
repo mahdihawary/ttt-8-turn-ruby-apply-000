58d7af9a86543aa6f19a1b5a492c7ab1b51df73b
Mahdi Hawary <mahdihawary@gmail.com>
2020-07-21 03:53:23 +0000

    Automatically backed up by Learn

---
+++ lib/turn.rb
@@ -0,0 +1,23 @@
+def valid_move?(board,index)
+    if !position_taken?(board,index) && index >= 0 && index <= 8
+       true
+    else
+        false
+    end
+end
+
+def position_taken? (board,index)
+    if board[index] == " " || board[index] == "" || board[index] == nil
+      false
+    elsif board[index] == "X" || board[index] == "O"
+      true
+    end
+end
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+   puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+   puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
